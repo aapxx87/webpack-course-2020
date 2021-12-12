dc11bf014a7e9de42ea02e938b94aa5ab357885b
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-12-12 22:58:22 +0300

    update

---
+++ src/analytics.js
@@ -0,0 +1,33 @@
+function createAnalytics() {
+
+    let counter = 0
+    let isDestroyed = false
+
+    const listener = () => counter++
+
+    document.addEventListener('click', listener)
+
+    return {
+        destroy() {
+            document.removeEventListener('click', listener)
+          },
+        getClicks() {
+              if(isDestroyed) {
+                  return 'An is destroyed'
+              }
+            return counter
+          }
+    }
+
+}
+
+
+window.analytics = createAnalytics()
+
+
+
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const post = new Post('Webpack post')
+
+console.log('Post to string:', post.toString())
